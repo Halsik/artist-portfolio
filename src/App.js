a710c2d040d31259de34557c78716079ddefe201
Michał Hals <michal.jan.hals@gmail.com>
2023-01-20 14:28:42 +0100

    hashrouter added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import HomePage from './components/HomePage/HomePage';
 import Contact from './components/ContactPage/Contact';
 import Portfolio from './components/PortfolioPage/Portfolio';
 import About from './components/AboutPage/About';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 
 
 
@@ -15,7 +15,7 @@ function App() {
 
   return (
     <div>
-      <BrowserRouter>
+      <HashRouter>
       <Routes>
          <Route path='/' element={<HomePage 
                                   theme={theme}/>} /> 
@@ -23,7 +23,7 @@ function App() {
          <Route path='/Portfolio' element={<Portfolio />} /> 
          <Route path='/Contact' element={<Contact />} /> 
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
